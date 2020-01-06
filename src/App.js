391624f53df9130f38723bc52175a3a044e0814a
tanvesh01 <sarvetanvesh01@gmail.com>
2020-01-06 15:44:44 +0530

    from pc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Layout from './components/Layout/Layout'
 import './App.css';
+import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder'
 
 function App() {
   return (
     <div>
       <Layout>
-        Yo Whatsapp
+        <BurgerBuilder />
       </Layout>
     </div>
   );
